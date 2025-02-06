e235f1510fefb573be7ff5d25b5c9d0a08c36cbb
gaby5u <gabrielaoloeri@gmail.com>
2025-02-06 02:18:29 +0200

    Create on hover fav-heart-icons

--- src/components/FavRecipe.jsx
+++ src/components/FavRecipe.jsx
@@ -16,7 +16,7 @@ const FavRecipe = ({ recipeId, image, title }) => {
         <img src={image} alt={title} className="rounded-t-[33px] w-full" />
         <div className="flex items-center justify-center rounded-full bg-white absolute top-2 right-2 w-[53px] h-[53px]">
           <svg
-            className="cursor-pointer fill-none hover:fill-red-500 hover:stroke-transparent transition-all duration-700"
+            className="cursor-pointer fill-transparent "
             width="29"
             height="29"
             viewBox="0 0 29 29"
@@ -26,6 +26,7 @@ const FavRecipe = ({ recipeId, image, title }) => {
               d="M14.4993 25.7979L12.7473 24.2029C6.52435 18.56 2.41602 14.8263 2.41602 10.2708C2.41602 6.53708 5.34018 3.625 9.06185 3.625C11.1643 3.625 13.1823 4.60375 14.4993 6.13833C15.8164 4.60375 17.8343 3.625 19.9368 3.625C23.6585 3.625 26.5827 6.53708 26.5827 10.2708C26.5827 14.8263 22.4743 18.56 16.2514 24.2029L14.4993 25.7979Z"
               stroke="#212121"
               strokeWidth="2"
+              className="transition-all duration-700 ease-in-out hover:stroke-none hover:fill-red-500 "
             />
           </svg>
         </div>
